96658e5e29d032091632940ed135d4c59b58c3de
BIGNIKi <nikita_sr1@mail.ru>
2021-11-05 17:13:17 +0700

    Added posibility to load sprites from tile maps

--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -3,23 +3,23 @@ package components;
 import job.Component;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
+import org.lwjgl.system.CallbackI;
 import renderer.Texture;
 
 public class SpriteRenderer extends Component
 {
     private Vector4f color;
-    private Vector2f[] texCoords;
-    private Texture texture;
+    private Sprite sprite;
 
     public SpriteRenderer(Vector4f color)
     {
         this.color = color;
-        this.texture = null;
+        this.sprite = new Sprite(null);
     }
 
-    public SpriteRenderer(Texture texture)
+    public SpriteRenderer(Sprite sprite)
     {
-        this.texture = texture;
+        this.sprite = sprite;
         this.color = new Vector4f(1,1,1,1);
     }
 
@@ -42,17 +42,11 @@ public class SpriteRenderer extends Component
 
     public Texture getTexture()
     {
-        return this.texture;
+        return sprite.getTexture();
     }
 
     public Vector2f[] getTexCoords()
     {
-        Vector2f[] texCoords = {
-                new Vector2f(1,1),
-                new Vector2f(1, 0),
-                new Vector2f(0,0),
-                new Vector2f(0,1)
-        };
-        return texCoords;
+        return sprite.getTexCoords();
     }
 }
