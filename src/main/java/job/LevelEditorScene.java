5a9c4001f1aa83a22521487fd93fb52c47b2eb27
BIGNIKi <nikita_sr1@mail.ru>
2021-11-08 20:37:19 +0700

    Objects serialization using GSON

--- src/main/java/job/LevelEditorScene.java
+++ src/main/java/job/LevelEditorScene.java
@@ -1,6 +1,8 @@
 package job;
 
 import Util.AssetPool;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import components.Sprite;
 import components.SpriteRenderer;
 import components.SpriteSheet;
@@ -24,31 +26,35 @@ public class LevelEditorScene extends Scene
         sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
 
         obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(121, 120)), 0);
-        obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
+        SpriteRenderer obj1SpriteRenderer = new SpriteRenderer();
+        obj1SpriteRenderer.setSprite(sprites.getSprite(0));
+        obj1.addComponent(obj1SpriteRenderer);
         this.addGameObjectToScene(obj1);
-
-        //GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(600, 100), new Vector2f(121, 120)));
-        //obj2.addComponent(new SpriteRenderer(sprites.getSprite(1)));
-        //this.addGameObjectToScene(obj2);
-
-
-        // Below is the way to load and draw particular image
-        /*GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(131, 132)));
-        obj1.addComponent(new SpriteRenderer(new Sprite(AssetPool.getTexture("assets/images/Stone0.png"))));
-        this.addGameObjectToScene(obj1);
-        */
 
         GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(100, 100)), 1);
-        obj2.addComponent(new SpriteRenderer(new Sprite(AssetPool.getTexture("assets/images/White.png"))));
+        SpriteRenderer obj2SpriteRenderer = new SpriteRenderer();
+        Sprite obj2Sprite = new Sprite();
+        obj2Sprite.setTexture(AssetPool.getTexture("assets/images/White.png"));
+        obj2SpriteRenderer.setSprite(obj2Sprite);
+        obj2.addComponent(obj2SpriteRenderer);
         this.addGameObjectToScene(obj2);
+
         GameObject obj3 = new GameObject("Object 3", new Transform(new Vector2f(450, 100), new Vector2f(100, 100)), 0);
-        obj3.addComponent(new SpriteRenderer(new Sprite(AssetPool.getTexture("assets/images/White.png"))));
+        SpriteRenderer obj3SpriteRenderer = new SpriteRenderer();
+        Sprite obj3Sprite = new Sprite();
+        obj3Sprite.setTexture(AssetPool.getTexture("assets/images/White.png"));
+        obj3SpriteRenderer.setSprite(obj3Sprite);
+        obj3.addComponent(obj3SpriteRenderer);
         this.addGameObjectToScene(obj3);
 
         this.activeGameObject = obj2;
 
         // the way to make colored rectangle without texture
         //obj1.addComponent(new SpriteRenderer(new Vector4f(1,0,0,1)));
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        System.out.println(gson.toJson(obj1));
     }
 
     private void loadResources()
