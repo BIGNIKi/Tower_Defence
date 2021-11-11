090332574975f416766b23240d7023fbe3f22c3d
BIGNIKi <nikita_sr1@mail.ru>
2021-11-11 13:32:45 +0700

    Added some bugs =)

--- src/main/java/job/MainWindow.java
+++ src/main/java/job/MainWindow.java
@@ -48,18 +48,17 @@ public final class MainWindow
         {
             case 0:
                 currentScene = new LevelEditorScene();
-                currentScene.init();
-                currentScene.start();
                 break;
             case 1:
                 currentScene = new LevelScene();
-                currentScene.init();
-                currentScene.start();
                 break;
             default:
                 assert false : "Unknown scene '" + newScene + "'";
                 break;
         }
+        currentScene.load();
+        currentScene.init();
+        currentScene.start();
     }
 
     //it is used for interaction with the class instance
@@ -175,8 +174,6 @@ public final class MainWindow
 
         int frameCount = 0;
         double previousTime = beginTime;
-
-        currentScene.load();
 
         while(!GLFW.glfwWindowShouldClose(_windowId)) //while window shouldn't be closed
         {
