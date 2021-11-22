9c04c78277f95127ad55686fe8ed9d147bab88ec
BIGNIKi <nikita_sr1@mail.ru>
2021-11-22 20:20:57 +0700

    Posibility to add new components

---
+++ src/main/java/components/TestComponent.java
@@ -0,0 +1,6 @@
+package components;
+
+public class TestComponent extends Component
+{
+
+}
---
+++ src/main/java/observers/events/Event.java
@@ -0,0 +1,26 @@
+package observers.events;
+
+import job.GameObject;
+import observers.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event
+{
+    private static List<Observer> observers = new ArrayList<>();
+
+    public static void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public static void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public static void notify(GameObject obj, Event event) {
+        for (Observer observer : observers) {
+            observer.onNotify(obj, event);
+        }
+    }
+}
