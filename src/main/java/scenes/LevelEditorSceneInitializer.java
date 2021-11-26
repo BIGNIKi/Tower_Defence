3a56b81074a6d28e495f31fcf1dd9166861e8b2a
BIGNIKi <nikita_sr1@mail.ru>
2021-11-26 22:57:09 +0700

    We have done REALLY GOOD AND EASY way to add textures into sprite's picking menu

--- src/main/java/scenes/LevelEditorSceneInitializer.java
+++ src/main/java/scenes/LevelEditorSceneInitializer.java
@@ -10,65 +10,81 @@ import job.*;
 import org.joml.Vector2f;
 import renderer.DebugDraw;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelEditorSceneInitializer extends SceneInitializer
 {
-    private SpriteSheet sprites;
+    //private SpriteSheet sprites;
     private GameObject levelEditorStuff;
 
+    private final List<Sprite> btnTexture = new ArrayList<>(); // все спрайты, которые нужно будет отобразить как кнопки
+
+    // этот метод запускается ПОСЛЕ loadResources
     @Override
     public void init(Scene scene)
     {
-        sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
         SpriteSheet gizmos = AssetPool.getSpritesheet("assets/images/gizmos.png");
 
-        levelEditorStuff = scene.createGameObject("LevelEditor");
-        levelEditorStuff.setNoSerialize();
-        levelEditorStuff.addComponent(new MouseControls());
-        levelEditorStuff.addComponent(new GridLines());
-        levelEditorStuff.addComponent(new EditorCamera(scene.camera()));
-        levelEditorStuff.addComponent(new GizmoSystem(gizmos));
+        levelEditorStuff = scene.createGameObject("LevelEditor"); // объект, который всегда висит на сцене
+        // и задействует следующие функции:
+        levelEditorStuff.setNoSerialize(); // этот объект не сохраняется
+        levelEditorStuff.addComponent(new MouseControls()); // отвечает за захват, размещение игрового объекта
+        levelEditorStuff.addComponent(new GridLines()); // рисует сетку
+        levelEditorStuff.addComponent(new EditorCamera(scene.camera())); // все взаимодействия с камерой для редактора
+        levelEditorStuff.addComponent(new GizmoSystem(gizmos)); // отрисовка стрелок для смены позиции/размера
         scene.addGameObjectToScene(levelEditorStuff);
 
         //DebugDraw.addLine2D(new Vector2f(0,0), new Vector2f(800, 800), new Vector3f(1, 1, 1), 120);
-
-/*        obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(121, 120)), 0);
-        SpriteRenderer obj1SpriteRenderer = new SpriteRenderer();
-        obj1SpriteRenderer.setSprite(sprites.getSprite(0));
-        obj1.addComponent(obj1SpriteRenderer);
-        this.addGameObjectToScene(obj1);
-        GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(100, 100)), 1);
-        SpriteRenderer obj2SpriteRenderer = new SpriteRenderer();
-        Sprite obj2Sprite = new Sprite();
-        obj2Sprite.setTexture(AssetPool.getTexture("assets/images/White.png"));
-        obj2SpriteRenderer.setSprite(obj2Sprite);
-        obj2.addComponent(obj2SpriteRenderer);
-        obj2.addComponent(new Rigidbody());
-        this.addGameObjectToScene(obj2);
-        GameObject obj3 = new GameObject("Object 3", new Transform(new Vector2f(450, 100), new Vector2f(100, 100)), 0);
-        SpriteRenderer obj3SpriteRenderer = new SpriteRenderer();
-        Sprite obj3Sprite = new Sprite();
-        obj3Sprite.setTexture(AssetPool.getTexture("assets/images/White.png"));
-        obj3SpriteRenderer.setSprite(obj3Sprite);
-        obj3.addComponent(obj3SpriteRenderer);
-        this.addGameObjectToScene(obj3);*/
 
         // the way to make colored rectangle without texture
         //obj1.addComponent(new SpriteRenderer(new Vector4f(1,0,0,1)));
     }
 
+    // перегоняет все спрайты из спрайтшита в List всех спрайтов btnTexture
+    private void spriteSheetToSprites(String path, int spriteWidth, int spriteHeight, int numSprites, int spacing)
+    {
+        // положили спрайты на видеокарту (теперь она знает о них)
+        AssetPool.addSpritesheet(path,
+                new SpriteSheet(AssetPool.getTexture(path),
+                        spriteWidth, spriteHeight, numSprites,spacing));
+        SpriteSheet sprites = AssetPool.getSpritesheet(path); // получили сам созданный спрайтshit
+        for(int i = 0; i < sprites.size(); i++)
+        {
+            Sprite sprite = sprites.getSprite(i);
+            btnTexture.add(sprite);
+        }
+
+    }
+
+    // загрузит текстуру в List всех спрайтов btnTexture
+    private void textureToSprite(String path)
+    {
+        Sprite sprite = new Sprite();
+        sprite.setTexture(AssetPool.getTexture(path));
+        btnTexture.add(sprite);
+    }
+
+    // эта функция запускается РАНЬШЕ чем init
     @Override
     public void loadResources(Scene scene)
     {
         AssetPool.getShader("assets/shaders/default.glsl");
 
-        AssetPool.addSpritesheet("assets/images/spritesheet.png",
-                new SpriteSheet(AssetPool.getTexture("assets/images/spritesheet.png"),
-                        121, 120, 2,0));
+        // START сюда пишутся текстуры, которые нужно видеть в редакторе выбора текстур
+
+        spriteSheetToSprites("assets/images/spritesheet.png", 121, 120, 2,0);
+        textureToSprite("assets/images/greenEnemy1.png");
+        textureToSprite("assets/images/base.png");
+
+        // STOP
+
         AssetPool.addSpritesheet("assets/images/gizmos.png",
                 new SpriteSheet(AssetPool.getTexture("assets/images/gizmos.png"),
                         24, 48, 3, 0));
-        //AssetPool.getTexture("assets/images/Stone0.png");
 
+
+        // проходит все объекты сцены, натягивая на них текстуры
         for(GameObject g : scene.getGameObjects())
         {
             if(g.getComponent(SpriteRenderer.class) != null)
@@ -89,9 +105,9 @@ public class LevelEditorSceneInitializer extends SceneInitializer
 
     //all bullshits bellow are for test new features
 
-    private int spriteIndex = 0;
+/*    private int spriteIndex = 0;
     private float spriteFlipTime = 0.3f;
-    private float spriteFlipTimeLeft = 0.0f;
+    private float spriteFlipTimeLeft = 0.0f;*/
     //float t = 0.0f;
 /*    @Override
     public void update(double dt)
@@ -121,6 +137,7 @@ public class LevelEditorSceneInitializer extends SceneInitializer
         }
     }*/
 
+    // метод вызывается каждый кадр (отвечает за вывод окошка с текстурами)
     @Override
     public void imgui()
     {
@@ -129,7 +146,7 @@ public class LevelEditorSceneInitializer extends SceneInitializer
         ImGui.end();
 
         ImGui.begin("Sprites:");
-/*        ImGui.text("Óðîí: 5000");
+/*       ImGui.text("Sample text = 5000");
         ImGui.text("HP: 1");*/
 
         ImVec2 windowPos = new ImVec2();
@@ -139,30 +156,25 @@ public class LevelEditorSceneInitializer extends SceneInitializer
         ImVec2 itemSpacing = new ImVec2();
         ImGui.getStyle().getItemSpacing(itemSpacing);
 
-        // TODO: óäàëèòü ïåðåìåííóþ
-        int tempI = 0;
-
         float windowX2 = windowPos.x + windowSize.x;
-        // ñîçäàíèå êíîïîê (òåêñòóð) â öèêëå
-        for(int i = 0; i < sprites.size(); i++)
+        // создание кнопок на котороые можно нажать, чтобы выбрать текстуру
+        for(int i = 0; i < btnTexture.size(); i++)
         {
-            Sprite sprite = sprites.getSprite(i);
-            //float spriteWidth = sprite.getWidth() * 4;
-            //float spriteHeight = sprite.getHeight() * 4;
-            float spriteWidth = 50;
-            float spriteHeight = 50;
+            Sprite sprite = btnTexture.get(i);
             int id = sprite.getTexId();
             Vector2f[] texCoords = sprite.getTexCoords();
 
             ImGui.pushID(i);
-            // óñëîâèÿ äëÿ êíîïîê
+            // само создание кнопок
+            // количество пикселей в ширину
+            float spriteWidth = 50;
+            // в длину
+            float spriteHeight = 50;
             if(ImGui.imageButton(id, spriteWidth, spriteHeight, texCoords[2].x, texCoords[0].y, texCoords[0].x, texCoords[2].y))
             {
-                //GameObject object = Prefabs.generateSpriteObject(sprite, sprite.getWidth(), sprite.getHeight());
                 GameObject object = Prefabs.generateSpriteObject(sprite, 0.25f, 0.25f);
                 // Attach this to the mouse cursor
                 // mouseControls.pickupObject(object);
-                // ÇÀÕÂÀÒ ÎÁÚÅÊÒÀ
                 levelEditorStuff.getComponent(MouseControls.class).pickupObject(object);
             }
             ImGui.popID();
@@ -171,58 +183,12 @@ public class LevelEditorSceneInitializer extends SceneInitializer
             ImGui.getItemRectMax(lastButtonPos);
             float lastButtonX2 = lastButtonPos.x;
             float nextButtonX2 = lastButtonX2 + itemSpacing.x + spriteWidth;
-            if(i + 1 < sprites.size() && nextButtonX2 < windowX2)
+            if(nextButtonX2 < windowX2)
             {
                 ImGui.sameLine();
             }
-            tempI = i;
         }
-
-        // ВРЕМЕННО
-
-        Sprite sprite = new Sprite();
-        sprite.setTexture(AssetPool.getTexture("assets/images/greenEnemy1.png"));
-        float spriteWidth = 50;
-        float spriteHeight = 50;
-        int id = sprite.getTexId();
-        Vector2f[] texCoords = sprite.getTexCoords();
-        ImGui.pushID(tempI);
-        tempI++;
-        // óñëîâèÿ äëÿ êíîïîê
-        if(ImGui.imageButton(id, spriteWidth, spriteHeight, texCoords[2].x, texCoords[0].y, texCoords[0].x, texCoords[2].y))
-        {
-            //GameObject object = Prefabs.generateSpriteObject(sprite, sprite.getWidth(), sprite.getHeight());
-            GameObject object = Prefabs.generateSpriteObject(sprite, 0.125f, 0.125f);
-            // Attach this to the mouse cursor
-            // mouseControls.pickupObject(object);
-            // ÇÀÕÂÀÒ ÎÁÚÅÊÒÀ
-            levelEditorStuff.getComponent(MouseControls.class).pickupObject(object);
-        }
-        ImGui.popID();
-        sprite = new Sprite();
-        sprite.setTexture(AssetPool.getTexture("assets/images/base.png"));
-        spriteWidth = 50;
-        spriteHeight = 50;
-        id = sprite.getTexId();
-        texCoords = sprite.getTexCoords();
-        ImGui.pushID(tempI);
-        tempI++;
-        // óñëîâèÿ äëÿ êíîïîê
-        if(ImGui.imageButton(id, spriteWidth, spriteHeight, texCoords[2].x, texCoords[0].y, texCoords[0].x, texCoords[2].y))
-        {
-            //GameObject object = Prefabs.generateSpriteObject(sprite, sprite.getWidth(), sprite.getHeight());
-            GameObject object = Prefabs.generateSpriteObject(sprite, 0.125f, 0.125f);
-            // Attach this to the mouse cursor
-            // mouseControls.pickupObject(object);
-            // ÇÀÕÂÀÒ ÎÁÚÅÊÒÀ
-            levelEditorStuff.getComponent(MouseControls.class).pickupObject(object);
-        }
-        ImGui.popID();
-
-        // ВРЕМЕННО
 
         ImGui.end();
     }
-
-    // TODO: ñäåëàòü íîðìàëüíî äîáàâëåíèå òåêñòóð
 }
